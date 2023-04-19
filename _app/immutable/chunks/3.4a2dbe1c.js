cf78daf74980de5f823f620f7ea6a461d8f67f5d
Denyl911 <imdenyl@gmail.com>
2023-04-19 12:53:17 -0600

    first commit

---
+++ _app/immutable/chunks/3.4a2dbe1c.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/quiz-page.svelte.4ff7d56a.js";export{t as component};
---
+++ _app/immutable/chunks/4.55a1cb9e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/resultados-page.svelte.d19d057a.js";export{t as component};
